3418e199232136463fe3b9ebe52fbcfa52b059b2
Hoà hoa hoè <97334361+tranhoa2003@users.noreply.github.com>
2024-10-16 09:20:56 +0700

    da

--- PROJECT/DB/restaurantdb.sql
+++ PROJECT/DB/restaurantdb.sql
@@ -3,15 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
-<<<<<<< HEAD
--- Generation Time: Sep 23, 2024 at 10:46 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.0.30
-=======
--- Generation Time: Sep 15, 2024 at 03:59 PM
+-- Generation Time: Oct 16, 2024 at 04:19 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
->>>>>>> 680ee18670dfceab7f0b659d8f149eee34a7d582
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -65,8 +59,16 @@ CREATE TABLE `dishes` (
 CREATE TABLE `dish_ingredients` (
   `dish_id` int(11) NOT NULL,
   `ingredient_id` int(11) NOT NULL,
-  `quantity` decimal(10,2) DEFAULT NULL
+  `quantity_required` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `dish_ingredients`
+--
+
+INSERT INTO `dish_ingredients` (`dish_id`, `ingredient_id`, `quantity_required`) VALUES
+(26, 11, NULL),
+(27, 10, NULL);
 
 -- --------------------------------------------------------
 
@@ -77,16 +79,17 @@ CREATE TABLE `dish_ingredients` (
 CREATE TABLE `ingredients` (
   `ingredient_id` int(11) NOT NULL,
   `ingredient_name` varchar(255) NOT NULL,
-  `quantity` int(11) NOT NULL
+  `quantity` int(11) NOT NULL,
+  `is_primary` tinyint(1) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `ingredients`
 --
 
-INSERT INTO `ingredients` (`ingredient_id`, `ingredient_name`, `quantity`) VALUES
-(2, 'thịt ba chỉ ', 10),
-(3, 'gà ', 100);
+INSERT INTO `ingredients` (`ingredient_id`, `ingredient_name`, `quantity`, `is_primary`) VALUES
+(10, 'phượng hoàng', 5, 0),
+(11, 'trứng gà', 5, 0);
 
 -- --------------------------------------------------------
 
@@ -98,7 +101,7 @@ CREATE TABLE `menu` (
   `dish_id` int(11) NOT NULL,
   `dish_name` varchar(100) NOT NULL,
   `dish_describe` text DEFAULT NULL,
-  `price` decimal(10,2) NOT NULL
+  `price` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -106,14 +109,8 @@ CREATE TABLE `menu` (
 --
 
 INSERT INTO `menu` (`dish_id`, `dish_name`, `dish_describe`, `price`) VALUES
-<<<<<<< HEAD
-(1, 'chicken Wing', 'gà ngon ', 20000),
-(2, 'gà đen', 'k ngon', 100000),
-(4, 'gà chiên ', 'gà chiên giòn', 10000);
-=======
-(1, 'chicken Wing', 'gà ngon ', 20000.00),
-(2, 'sodfhohi', 'k ngon', 100000.00);
->>>>>>> 680ee18670dfceab7f0b659d8f149eee34a7d582
+(26, 'trứng rán', 'hành', 1212),
+(27, 'phượng hoàng gián', 'ngon', 2324);
 
 -- --------------------------------------------------------
 
@@ -122,46 +119,47 @@ INSERT INTO `menu` (`dish_id`, `dish_name`, `dish_describe`, `price`) VALUES
 --
 
 CREATE TABLE `orders` (
-  `id` int(11) NOT NULL,
-  `table_id` int(11) NOT NULL,
-  `dish_id` int(11) NOT NULL,
-  `quantity` int(11) DEFAULT 1
+  `order_id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `total_price` decimal(10,2) DEFAULT NULL,
+  `order_date` datetime DEFAULT NULL,
+  `invoice_code` varchar(255) NOT NULL,
+  `order_code` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `orders`
 --
 
-INSERT INTO `orders` (`id`, `table_id`, `dish_id`, `quantity`) VALUES
-<<<<<<< HEAD
-(5, 1, 1, 1),
-(6, 5, 1, 1),
-(7, 5, 2, 1),
-(8, 8, 1, 1),
-(10, 2, 1, 1),
-(11, 2, 2, 1);
-=======
-(6, 5, 1, 1),
-(7, 5, 2, 1),
-(19, 2, 1, 2),
-(20, 2, 2, 1),
-(31, 21, 1, 1),
-(38, 1, 1, 6),
-(39, 1, 2, 1);
->>>>>>> 680ee18670dfceab7f0b659d8f149eee34a7d582
+INSERT INTO `orders` (`order_id`, `user_id`, `total_price`, `order_date`, `invoice_code`, `order_code`) VALUES
+(1, NULL, 620.00, '2024-09-24 01:57:39', '', NULL),
+(2, NULL, 300.00, '2024-09-24 01:58:43', '', NULL),
+(3, NULL, 340.00, '2024-09-24 02:02:17', '', NULL),
+(4, 3, 960.00, '2024-09-28 12:03:50', '', NULL),
+(5, 3, 20.00, '2024-09-28 12:08:12', '', NULL),
+(6, 4, 500.00, '2024-09-28 12:08:39', '', NULL),
+(7, 3, 200.00, '2024-09-28 12:09:03', '', NULL),
+(8, 3, 800.00, '2024-09-28 12:16:57', '', NULL),
+(9, 3, 500.00, '2024-09-30 23:15:55', '', NULL),
+(10, 3, 300.00, '2024-09-30 23:22:07', 'INV66FAD02F853EF', NULL),
+(11, 3, 400.00, '2024-09-30 23:24:00', 'INV66FAD0A0A85E1', NULL),
+(14, 3, 2540.00, '2024-09-30 23:51:25', '', NULL),
+(15, 3, 120.00, '2024-10-03 11:25:35', '', NULL),
+(22, 5, 3.00, '2024-10-13 00:19:52', '', NULL);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `order_history`
+-- Table structure for table `order_items`
 --
 
-CREATE TABLE `order_history` (
-  `order_id` int(11) NOT NULL,
-  `table_id` int(11) DEFAULT NULL,
-  `dish_id` int(11) DEFAULT NULL,
-  `total` decimal(10,2) DEFAULT NULL,
-  `status` varchar(50) DEFAULT NULL
+CREATE TABLE `order_items` (
+  `order_item_id` int(11) NOT NULL,
+  `order_id` int(11) DEFAULT NULL,
+  `dish_name` varchar(255) DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT NULL,
+  `dish_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -181,26 +179,32 @@ CREATE TABLE `restaurant_table` (
 --
 
 INSERT INTO `restaurant_table` (`table_id`, `table_number`, `status`) VALUES
-(1, 1, 'occupied'),
-(2, 2, 'occupied'),
-(3, 3, 'occupied'),
-(4, 4, 'occupied'),
-(5, 5, 'occupied'),
-(6, 6, 'occupied'),
-(7, 7, 'occupied'),
-(8, 8, 'occupied'),
-(9, 9, 'occupied'),
-(10, 10, 'occupied'),
-(11, 11, 'occupied'),
-(12, 12, 'occupied'),
-<<<<<<< HEAD
-(13, 13, 'occupied'),
-(14, 14, 'occupied'),
-(18, 77, 'occupied'),
-(19, 22, 'occupied');
-=======
-(21, 14, 'occupied');
->>>>>>> 680ee18670dfceab7f0b659d8f149eee34a7d582
+(27, 1, 'empty'),
+(28, 2, 'empty'),
+(29, 3, 'empty');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `table_orders`
+--
+
+CREATE TABLE `table_orders` (
+  `table_id` int(11) DEFAULT NULL,
+  `dish_id` int(11) DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `table_orders`
+--
+
+INSERT INTO `table_orders` (`table_id`, `dish_id`, `quantity`) VALUES
+(25, 9, 1),
+(25, 10, 1),
+(25, 14, 1),
+(3, 9, 3),
+(3, 17, 1);
 
 -- --------------------------------------------------------
 
@@ -211,32 +215,21 @@ INSERT INTO `restaurant_table` (`table_id`, `table_number`, `status`) VALUES
 CREATE TABLE `user` (
   `user_id` int(11) NOT NULL,
   `user_name` varchar(100) NOT NULL,
-<<<<<<< HEAD
   `phone_number` int(11) DEFAULT NULL,
   `email` varchar(100) DEFAULT NULL,
   `password` text NOT NULL,
   `role` int(11) NOT NULL
-=======
-  `phone_number` varchar(15) DEFAULT NULL,
-  `email` varchar(100) DEFAULT NULL,
-  `password` text NOT NULL
->>>>>>> 680ee18670dfceab7f0b659d8f149eee34a7d582
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `user`
 --
 
-<<<<<<< HEAD
 INSERT INTO `user` (`user_id`, `user_name`, `phone_number`, `email`, `password`, `role`) VALUES
 (2, 'vu01', 774303475, 'nguyenvu00304@gmail.com', '$2y$10$wbLfwustkWn9l9ptvZhih.Zw1lRbBAxWtQe9UmYxjh3jCUFvC7Mve', 1),
 (3, 'vu02', 2147483647, 'pimpompimpom4@gmail.com', '$2y$10$buj6FdOyU050oMJ/2.D4iuJms.WPjV0BrH5FxjovAfZ2GZXasweyW', 0),
-(4, 'vu03', 2147483647, 'aoiuhfds@gmail.com', '$2y$10$WUpUXjQ5TlG/XJSq5VUYr.N/LU9QVVC4cmB/8wscfPJcre2CovanC', 0);
-=======
-INSERT INTO `user` (`user_id`, `user_name`, `phone_number`, `email`, `password`) VALUES
-(2, 'vu01', '0774303475', 'nguyenvu00304@gmail.com', '$2y$10$wbLfwustkWn9l9ptvZhih.Zw1lRbBAxWtQe9UmYxjh3jCUFvC7Mve'),
-(3, 'vu02', '09378402378045', 'pimpompimpom4@gmail.com', '$2y$10$buj6FdOyU050oMJ/2.D4iuJms.WPjV0BrH5FxjovAfZ2GZXasweyW');
->>>>>>> 680ee18670dfceab7f0b659d8f149eee34a7d582
+(4, 'vu03', 2147483647, 'aoiuhfds@gmail.com', '$2y$10$WUpUXjQ5TlG/XJSq5VUYr.N/LU9QVVC4cmB/8wscfPJcre2CovanC', 0),
+(5, 'vugiau', 123456789, 'hoa@gmail.com', '$2y$10$yQWqMzTQ4pVBC31Nafhu0e0sNd0vI5Vdz0xeKTLkIP7XSiuXDkDX6', 0);
 
 --
 -- Indexes for dumped tables
@@ -278,17 +271,15 @@ ALTER TABLE `menu`
 -- Indexes for table `orders`
 --
 ALTER TABLE `orders`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `table_id` (`table_id`),
-  ADD KEY `dish_id` (`dish_id`);
+  ADD PRIMARY KEY (`order_id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
--- Indexes for table `order_history`
+-- Indexes for table `order_items`
 --
-ALTER TABLE `order_history`
-  ADD PRIMARY KEY (`order_id`),
-  ADD KEY `table_id` (`table_id`),
-  ADD KEY `dish_id` (`dish_id`);
+ALTER TABLE `order_items`
+  ADD PRIMARY KEY (`order_item_id`),
+  ADD KEY `order_id` (`order_id`);
 
 --
 -- Indexes for table `restaurant_table`
@@ -322,51 +313,37 @@ ALTER TABLE `dishes`
 -- AUTO_INCREMENT for table `ingredients`
 --
 ALTER TABLE `ingredients`
-  MODIFY `ingredient_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `ingredient_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `menu`
 --
 ALTER TABLE `menu`
-  MODIFY `dish_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `dish_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
--- AUTO_INCREMENT for table `orders`
+-- AUTO_INCREMENT for table `order_items`
 --
-ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
-
---
--- AUTO_INCREMENT for table `order_history`
---
-ALTER TABLE `order_history`
-  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `order_items`
+  MODIFY `order_item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
 
 --
 -- AUTO_INCREMENT for table `restaurant_table`
 --
 ALTER TABLE `restaurant_table`
-<<<<<<< HEAD
-  MODIFY `table_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
-=======
-  MODIFY `table_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
->>>>>>> 680ee18670dfceab7f0b659d8f149eee34a7d582
+  MODIFY `table_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-<<<<<<< HEAD
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-=======
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
->>>>>>> 680ee18670dfceab7f0b659d8f149eee34a7d582
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Constraints for dumped tables
@@ -379,7 +356,6 @@ ALTER TABLE `booking_history`
   ADD CONSTRAINT `booking_history_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
 
 --
-<<<<<<< HEAD
 -- Constraints for table `dishes`
 --
 ALTER TABLE `dishes`
@@ -389,24 +365,20 @@ ALTER TABLE `dishes`
 -- Constraints for table `dish_ingredients`
 --
 ALTER TABLE `dish_ingredients`
-  ADD CONSTRAINT `dish_ingredients_ibfk_1` FOREIGN KEY (`dish_id`) REFERENCES `dishes` (`dish_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `dish_ingredients_ibfk_2` FOREIGN KEY (`ingredient_id`) REFERENCES `ingredients` (`ingredient_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `dish_ingredients_ibfk_1` FOREIGN KEY (`dish_id`) REFERENCES `menu` (`dish_id`),
+  ADD CONSTRAINT `dish_ingredients_ibfk_2` FOREIGN KEY (`ingredient_id`) REFERENCES `ingredients` (`ingredient_id`);
 
 --
-=======
->>>>>>> 680ee18670dfceab7f0b659d8f149eee34a7d582
 -- Constraints for table `orders`
 --
 ALTER TABLE `orders`
-  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`table_id`) REFERENCES `restaurant_table` (`table_id`),
-  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`dish_id`) REFERENCES `menu` (`dish_id`);
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE;
 
 --
--- Constraints for table `order_history`
+-- Constraints for table `order_items`
 --
-ALTER TABLE `order_history`
-  ADD CONSTRAINT `order_history_ibfk_1` FOREIGN KEY (`table_id`) REFERENCES `restaurant_table` (`table_id`),
-  ADD CONSTRAINT `order_history_ibfk_2` FOREIGN KEY (`dish_id`) REFERENCES `menu` (`dish_id`);
+ALTER TABLE `order_items`
+  ADD CONSTRAINT `order_items_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
